6886a97e10e4e24760dd6a3fb79a59a05d3f3f38
zhangzhiqing <2515419313@qq.com>
2020-07-28 16:15:25 +0800

    解决跨域问题

--- order_boot/src/main/java/com/work/order/OrderApplication.java
+++ order_boot/src/main/java/com/work/order/OrderApplication.java
@@ -5,7 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderApplication {
-
+//	@Bean
+//	public FilterRegistrationBean httpServletRequestReplacedRegistration() {
+//		FilterRegistrationBean registration = new FilterRegistrationBean();
+//		registration.setFilter(new HttpServletRequestReplacedFilter());
+//		registration.addUrlPatterns("/*");
+//		registration.addInitParameter("paramName", "paramValue");
+//		registration.setName("httpServletRequestReplacedFilter");
+//		registration.setOrder(1);
+//		return registration;
+//	}
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
 	}
