e37b92bab6d21ee643f7888fe818124396cc2df1
zhangzhiqing <2515419313@qq.com>
2020-07-30 16:37:09 +0800

    提交

--- order_web/src/views/content/orderList.vue
+++ order_web/src/views/content/orderList.vue
@@ -1,12 +1,75 @@
 <template>
-    <div>
-      我是新打开的额页面
-    </div>
+  <el-container>
+    <el-header height="50px" style="padding-top:10px" >
+      <el-breadcrumb separator-class="el-icon-arrow-right" style="font-size: 15px">
+        <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+      </el-breadcrumb>
+    </el-header>
+    <el-main>
+      <el-table :data="tableData" border stripe size="mini" height="100%" style="width:100%">
+        <el-table-column fixed prop="date" label="日期" width="150"></el-table-column>
+        <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+        <el-table-column prop="province" label="省份" width="120"></el-table-column>
+        <el-table-column prop="city" label="市区" width="120"></el-table-column>
+        <el-table-column prop="address" label="地址" width="300"></el-table-column>
+        <el-table-column prop="zip" label="邮编" width="120"></el-table-column>
+        <el-table-column fixed="right" label="操作" width="100"><template slot-scope="scope">
+        <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+        <el-button type="text" size="small">编辑</el-button></template>
+        </el-table-column>
+      </el-table>
+      <el-footer style="text-align:right;padding-top:20px;height:70px;">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage4"
+          :page-sizes="[5, 10, 15, 20]"
+          :page-size="100"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="100">
+        </el-pagination>
+      </el-footer>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
     export default {
-        name: "order-list"
+        name: "order-list",
+        data() {
+          return {
+            tableData: [{
+              date: '2016-05-02',
+              name: '王小虎',
+              province: '上海',
+              city: '普陀区',
+              address: '上海市普陀区金沙江路 1518 弄',
+              zip: 200333
+            }, {
+              date: '2016-05-04',
+              name: '王小虎',
+              province: '上海',
+              city: '普陀区',
+              address: '上海市普陀区金沙江路 1517 弄',
+              zip: 200333
+            }, {
+              date: '2016-05-01',
+              name: '王小虎',
+              province: '上海',
+              city: '普陀区',
+              address: '上海市普陀区金沙江路 1519 弄',
+              zip: 200333
+            }, {
+              date: '2016-05-03',
+              name: '王小虎',
+              province: '上海',
+              city: '普陀区',
+              address: '上海市普陀区金沙江路 1516 弄',
+              zip: 200333
+            }]
+          }
+      },
     }
 </script>
 
--- order_web/src/views/home/index.vue
+++ order_web/src/views/home/index.vue
@@ -85,17 +85,6 @@
       <el-main style="height:100%;overflow:hidden;padding:0 10px;">
         <router-view></router-view>
       </el-main>
-      <el-footer>
-        <el-pagination
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          :current-page="currentPage4"
-          :page-sizes="[100, 200, 300, 400]"
-          :page-size="100"
-          layout="total, sizes, prev, pager, next, jumper"
-          :total="400">
-        </el-pagination>
-      </el-footer>
     </el-container>
   </el-container>
 </template>
